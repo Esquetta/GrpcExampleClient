e49adb25ea1e31091a298f6132fcea35013d849d
Esquetta <esquetta@gmail.com>
2023-08-13 16:22:08 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿
+using Grpc.Net.Client;
+using GrpcClient;
+
+var channel = GrpcChannel.ForAddress("http://localhost:5047");
+var client = new Greeter.GreeterClient(channel);
+HelloReply response = await client.SayHelloAsync(new HelloRequest { Name = "sazan" });
+Console.WriteLine($"Response:{response.Message}");
